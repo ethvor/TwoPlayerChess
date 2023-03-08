7834fb503fc8170136839f6c07583a251154c345
ethvor <118936291+ethvor@users.noreply.github.com>
2023-03-08 04:53:15 -0500

    gui displays the 12 custom 32bit pieces i made in an arbitrary position, has a checkerboard pattern. this took a long time... now i have to figure out when to render multiple of one type of piece, and get the ideal coordinates in every square for rendering a given piece.

--- Graphics.py
+++ Graphics.py
@@ -1,8 +1,15 @@
 import customtkinter as ctk
+import tkinter as tk
 import numpy as np
 from PIL import Image, ImageTk
+import os
+
+ROOT_DIR_BACKSLASH = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = ROOT_DIR_BACKSLASH.replace("\\", "/")
 
 
+# taken from https://stackoverflow.com/questions/14910858/how-to-specify-where-a-tkinter-window-opens
+# author: xxmbabanexx (Feb 16, 2013 at 16:47)
 def center_window(width, height, window):
     # get screen width and height
     screen_width = window.winfo_screenwidth()
@@ -61,10 +68,12 @@ def pageTwoGui(p1name, p2name, window):
     label = ctk.CTkLabel(master=frame, text="Who will go first?", font=("Roboto", 24))
     label.pack(pady=15, padx=10)
 
-    player1button = ctk.CTkButton(master=frame, text=str(p1name), command=lambda: startGame(p1name, p2name, window, frame))
+    player1button = ctk.CTkButton(master=frame, text=str(p1name),
+                                  command=lambda: startGame(p1name, p2name, window, frame))
     player1button.pack(pady=15, padx=10)
 
-    player2button = ctk.CTkButton(master=frame, text=str(p2name), command=lambda: startGame(p2name, p1name, window, frame))
+    player2button = ctk.CTkButton(master=frame, text=str(p2name),
+                                  command=lambda: startGame(p2name, p1name, window, frame))
     player2button.pack(pady=15, padx=10)
 
     randomButton = ctk.CTkButton(master=frame, text="Random", command=lambda: coinflip(p1name, p2name, window, frame),
@@ -84,13 +93,74 @@ def startGame(whitePlayerName, blackPlayerName, window, frame):
     frame.destroy()
     screen_width = window.winfo_screenwidth()
     screen_height = window.winfo_screenheight()
-    center_window(screen_width - (screen_width / 6), screen_height - (screen_height / 6), window)
-    gameGui(whitePlayerName, blackPlayerName, window)
+    gameWindowWidth = screen_width - (screen_width / 6)
+    gameWindowHeight = screen_height - (screen_height / 6)
+    center_window(gameWindowWidth, gameWindowHeight, window)
+    gameGui(window, whitePlayerName, blackPlayerName, gameWindowWidth, gameWindowHeight)
 
 
-def gameGui(window, whitePlayerName="Player 1", blackPlayerName="Player 2"):
-    frame = ctk.CTkFrame(master=window)
-    frame.pack(pady=50, padx=20, fill="both", expand=True)
+def gameGui(window, whitePlayerName, blackPlayerName, gameWindowWidth, gameWindowHeight):
+    frame = ctk.CTkFrame(master=window, bg_color="darkgray", fg_color="darkgray")
+    renderSquares(window, frame, gameWindowWidth, gameWindowHeight)
+
+
+def renderSquares(window, frame, gameWindowWidth, gameWindowHeight):
+    canvas_height = gameWindowHeight * 0.85
+    canvas_width = canvas_height
+    canvas = ctk.CTkCanvas(master=frame, width=canvas_width, height=canvas_height)
+
+    color = 'gray'
+
+    squaresize = canvas_height / 8
+
+    # got inspiration for this loop below to render a checkerboard from stackoverflow
+    # https://stackoverflow.com/questions/59356899/create-checkerboard-pattern-with-python-canvas
+    # modified it slightly, but its close enough to credit the author, "furas" (Dec 16, 2019 at 13:08)
+
+    for i in range(8):
+        for j in range(8):
+            corner1 = j * squaresize
+            corner2 = i * squaresize
+            corner3 = corner1 + squaresize
+            corner4 = corner2 + squaresize
+            canvas.create_rectangle((corner1, corner2, corner3, corner4), fill=color)
+            if color == 'gray':
+                color = 'lightgray'
+            else:
+                color = 'gray'
+
+        if color == 'gray':
+            color = 'lightgray'
+        else:
+            color = 'gray'
+
+    canvas.pack(expand=True)
+    frame.pack(pady=50, padx=50, fill="both", expand=True)
+    renderPieces(canvas, window)
+
+
+def imageRender(x, y, canvas, window, filename):
+    path = "/Users\ofzel/PycharmProjects/Chess/" + filename
+    print(path)
+    unconvertedImage = Image.open(path)
+    convertedImage = ImageTk.PhotoImage(unconvertedImage)
+    ctkImage = ctk.CTkImage(unconvertedImage)
+    canvas.create_image(x, y, image=convertedImage)
+    label = ctk.CTkLabel(window, image=ctkImage)
+    label.image = convertedImage
+
+
+def renderPieces(canvas, window):
+    renderPieceList = ["whiteBishop", "blackBishop", "whiteKing", "blackKing", "whiteKnight", "blackKnight",
+                       "whiteQueen",
+                       "blackQueen", "whiteRook", "blackRook", "whitePawn", "blackPawn"]
+
+    for piece in renderPieceList:
+        filename = piece + ".png"
+        coordx = renderPieceList.index(piece) * 50 + 10
+        # this just splays them out so i can see that they're all there YAY
+        coordy = coordx
+        imageRender(coordx, coordy, canvas, window, filename)
+
 
 initialGui()
-
